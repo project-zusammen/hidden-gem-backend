b3127dc4f8a923a00cb33bc83b6c1d13a3597136
Fatma Janna <fatma.janna@quinyx.com>
2023-12-20 08:12:43 +0700

    update review model with user_id

--- app/main/model/review.py
+++ app/main/model/review.py
@@ -1,15 +1,15 @@
 from .. import db
 
 class Review(db.Model):
-    __tablename__ = "review"
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     public_id = db.Column(db.String(100), unique=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     region_id = db.Column(db.Integer, db.ForeignKey('region.id'), nullable=False)
-    title = db.Column(db.String(100))
-    content = db.Column(db.String(255))
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
@@ -18,9 +18,9 @@ class Review(db.Model):
     visible = db.Column(db.Boolean, nullable=False, default=True)
 
 
-    def __init__(self, public_id, category_id, region_id, title, content, location, created_at, updated_at, upvotes, downvotes, visible):
+    def __init__(self, public_id, user_id, category_id, region_id, title, content, location, created_at, updated_at, upvotes, downvotes, visible):
         self.public_id = public_id
-        # self.user_id = user_id
+        self.user_id = user_id
         self.category_id = category_id
         self.region_id = region_id
         self.title = title
@@ -41,7 +41,7 @@ class Review(db.Model):
         return {
             'id': self.id,
             'public_id': self.public_id,
-            # 'user_id': self.user_id,
+            'user_id': self.user_id,
             'category_id': self.category_id,
             'region_id': self.region_id,
             'title': self.title,
