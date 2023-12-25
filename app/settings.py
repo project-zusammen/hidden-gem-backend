4890714f2af55502b68a48cbe82f853032e4dadd
Fatma Janna <fatma.janna@quinyx.com>
2023-12-25 14:08:57 +0700

    update settings n add test settings

--- app/settings.py
+++ app/settings.py
@@ -7,17 +7,20 @@ db_host = env.str("DB_HOST")
 db_user = env.str("DB_USER")
 db_password = env.str("DB_PASSWORD")
 db_port = env.str("DB_PORT")
+db_name = env.str("DEFAULT_DB_NAME")
 debug_mode = env.str("DEBUG")
 
-DATABASE_URI = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/defaultdb"
+def get_db_uri(db_name):
+    return f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+DATABASE_URI = get_db_uri(db_name)
 
 ENV = env.str("FLASK_ENV", default="production")
-print('ENV: ', ENV)
-DEBUG = ENV == "development"
+DEBUG = ENV == "dev"
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SECRET_KEY = env.str("SECRET_KEY")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
-CACHE_TYPE = "SimpleCache"  # Can be "MemcachedCache", "RedisCache", etc.
+CACHE_TYPE = "SimpleCache"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
