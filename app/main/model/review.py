805944c5ad72b0eb252b41e0ec85ac31b7947c15
Fatma Janna <fatma.janna@quinyx.com>
2023-12-25 13:51:41 +0700

    refactor review model and update utc to local time

--- app/main/model/review.py
+++ app/main/model/review.py
@@ -1,5 +1,7 @@
 import uuid
+import datetime
 from .. import db
+from ..util.helper import convert_to_local_time
 
 class Review(db.Model):
     __tablename__ = "review"
@@ -19,26 +21,13 @@ class Review(db.Model):
     visible = db.Column(db.Boolean, nullable=False, default=True)
 
 
-    def __init__(self, public_id, title, content, location, created_at, updated_at, upvotes=0, downvotes=0, visible=True):
-        self.public_id = public_id
-        # self.user_id = user_id
-        # self.category_id = category_id
-        # self.region_id = region_id
-        self.title = title
-        self.content = content
-        self.location = location
-        self.created_at = created_at
-        self.updated_at = updated_at
-        self.upvotes = upvotes
-        self.downvotes = downvotes
-        self.visible = visible
-    
-
     def __repr__(self):
         return f"<Review(title={self.title}, content={self.content})>"
     
 
     def serialize(self):
+        created_at = convert_to_local_time(self.created_at)
+        updated_at = convert_to_local_time(self.updated_at)
         return {
             'public_id': self.public_id,
             # 'user_id': self.user_id,
@@ -47,8 +36,8 @@ class Review(db.Model):
             'title': self.title,
             'content': self.content,
             'location': self.location,
-            'created_at': self.created_at.isoformat() if self.created_at else None,
-            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
+            'created_at': created_at.isoformat() if self.created_at else None,
+            'updated_at': updated_at.isoformat() if self.updated_at else None,
             'upvotes': self.upvotes,
             'downvotes': self.downvotes,
             'visible': self.visible
@@ -58,3 +47,52 @@ class Review(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+
+    def get_all_reviews(self):
+        return self.query.filter_by(visible=True).all()
+
+
+    def get_review_by_id(self, public_id):
+        return self.query.filter_by(public_id=public_id, visible=True).first()
+
+
+    def create_review(self, data):
+        review = Review(
+            public_id=str(uuid.uuid4()),
+            # user_id=data.get('user_id'),
+            # category_id=data.get('category_id'),
+            # region_id=data.get('region_id'),
+            title=data.get('title'),
+            content=data.get('content'),
+            location=data.get('location'),
+            created_at=datetime.datetime.utcnow(),
+            updated_at=datetime.datetime.utcnow(),
+        )
+        review.save()
+        return review
+
+
+    def update_review(self, public_id, data):
+        review = self.get_review_by_id(public_id)
+        if not review:
+            return None
+        else:
+            review.category_id = data.get('category_id')
+            review.region_id = data.get('region_id')
+            review.title = data.get('title')
+            review.content = data.get('content')
+            review.location = data.get('location')
+            review.updated_at = datetime.datetime.utcnow()
+            review.save()
+            return review
+    
+
+    def delete_review(self, public_id):
+        review = self.get_review_by_id(public_id)
+        if not review:
+            return None
+        else:
+            review.visible = False
+            review.save()
+            return review
---
+++ app/main/util/helper.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+import time
+
+def convert_to_local_time(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    return utc_datetime + offset
