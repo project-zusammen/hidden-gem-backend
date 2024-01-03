6b9d2f2bca06fa6cfa8a92f723b5f25964c460bc
Achmadqizwini <akise.shun@gmail.com>
2024-01-03 13:57:10 +0700

    Add: User authentication endpoint

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -39,13 +39,6 @@ class UserDto:
             "email": fields.String(required=True, description="user email"),
             "password": fields.String(required=True, description="user password")
         },
-    ),
-    login = api.model(
-        "login",
-        {
-            "email": fields.String(required=True, description="user email for login"),
-            "password": fields.String(required=True, description="user password for login")
-        },
     )
     login = api.model(
         "login",
@@ -60,3 +53,14 @@ class UserDto:
             "banned": fields.Boolean(required=True, description="user status that want to be updated"),
         }
     )
+
+class LoginDto:
+    login = api.model(
+        "login",
+        {
+            "email": fields.String(required=True, description="user email for login"),
+            "password": fields.String(required=True, description="user password for login")
+        },
+    )
+
+
