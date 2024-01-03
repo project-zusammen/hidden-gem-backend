ffbf4faaad576f0fe27a5061b69e87832e375423
Achmadqizwini <akise.shun@gmail.com>
2024-01-03 13:58:17 +0700

    Add: decorator for verifying jwt

--- app/main/util/token_verify.py
+++ app/main/util/token_verify.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from flask import request, jsonify
 from functools import wraps
-from app.main.model.user import User
 
 load_dotenv()
 
@@ -11,28 +10,25 @@ secretKey = os.getenv("SECRET_KEY")
 
 # decorator for verifying the JWT
 def token_required(f):
-
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        # jwt is passed in the request header
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        # return 401 if token is not passed
+        if 'X-API-KEY' in request.headers:
+            token = request.headers.get('X-API-KEY')
         if not token:
-            return jsonify({'message' : 'Token is missing !!'}), 401
-  
+            return {'message': 'Token is missing !!'}, 401
+
         try:
             # decoding the payload to fetch the stored details
-            data = jwt.decode(token, secretKey)
-            current_user = User.query\
-                .filter_by(public_id = data['public_id'])\
-                .first()
-        except:
-            return jsonify({
-                'message' : 'Token is invalid !!'
-            }), 401
-        # returns the current logged in users context to the routes
-        return  f(current_user, *args, **kwargs)
-  
+            data = jwt.decode(token, secretKey, algorithms=["HS256"])
+            
+            return f(decoded_token=data,*args, **kwargs)
+
+        except jwt.ExpiredSignatureError:
+            return {'message': 'Token has expired !!'}, 401
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return {'message': 'Token is invalid !!'}, 401
+
     return decorated
+
