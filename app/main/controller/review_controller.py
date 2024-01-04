672aa2bae5a453fad5aa0883c5593e95a7103acd
hfzfdlh <fadillahhafiz.fadillah@gmail.com>
2024-01-04 19:51:52 +0700

    add upvote endpoint and visible endpoint

--- app/main/controller/review_controller.py
+++ app/main/controller/review_controller.py
@@ -2,6 +2,8 @@ from ..util.dto import ReviewDto
 
 review_dto = ReviewDto()
 _review = review_dto.review
+_upvote = review_dto.upvote
+_visible = review_dto.visible
 
 from flask_restx import Resource
 from ..service.review_service import (
@@ -10,6 +12,8 @@ from ..service.review_service import (
     get_a_review,
     update_review,
     delete_review,
+    upvote_review,
+    update_visibility
 )
 from ...extensions import ns
 
@@ -43,3 +47,24 @@ class Review(Resource):
     def delete(self, public_id):
         """Delete a review"""
         return delete_review(public_id)
+
+
+@ns.route("/review/<public_id>/vote")
+@ns.param("public_id", "The Review Identifier")
+class ReviewUpvote(Resource):    
+    @ns.expect(_upvote)
+    def put(self, public_id):
+        """Give upvote or downvote"""
+        upvote = ns.payload.get("upvote")
+        upvoted_review = upvote_review(public_id, upvote)
+        return upvoted_review
+
+@ns.route("/review/<public_id>/status")
+@ns.param("public_id", "The Review Identifier")
+class ReviewVisible(Resource):    
+    @ns.expect(_visible)
+    def put(self, public_id):
+        """Give visibility status"""
+        visible = ns.payload.get("visible")
+        updated_visibility= update_visibility(public_id, visible)
+        return updated_visibility
