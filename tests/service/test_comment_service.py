6e83d08987f40e69c4f57f408e65713423b5c480
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-01-24 06:37:27 +0900

    several test comment service

--- tests/service/test_comment_service.py
+++ tests/service/test_comment_service.py
@@ -4,7 +4,8 @@ from unittest.mock import patch
 from app import create_app
 from app.main.service.comment_service import (
     get_all_comments,
-    get_a_comment
+    get_a_comment,
+    create_comment
 )
 
 def generate_fake_public_id():
@@ -66,3 +67,23 @@ class TestReviewService(TestCase):
         self.assertEqual(result["review_id"], data["review_id"])
 
         mock_get_comment_by_id.assert_called_once()
+
+    @patch("app.main.model.comment.Comment.create_comment")
+    def test_create_comment(self, mock_create_comment):
+        # ARRANGE
+        data = comment_1
+        mock_create_comment.return_value = data
+
+        # ACT
+        response, status_code = create_comment(data)
+        result = response["data"]
+
+        # ASSERT
+        self.assertEqual(status_code, 201)
+        self.assertEqual(response["status"], "success")
+        self.assertEqual(response["message"], "Successfully created.")
+        self.assertEqual(result["public_id"], data["public_id"])
+        self.assertEqual(result["content"], data["content"])
+        self.assertEqual(result["review_id"], data["review_id"])
+
+        mock_create_comment.assert_called_once()
