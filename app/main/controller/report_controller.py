909f074d869e575a39cb76318de331f439de7ece
Fatma Janna <fatma.janna@quinyx.com>
2024-01-29 09:59:31 +0700

    add report controller with req_token

--- app/main/controller/report_controller.py
+++ app/main/controller/report_controller.py
@@ -2,6 +2,7 @@ from ..util.dto import ReportDto
 from flask_restx import Resource
 from ...extensions import ns
 from ..service.report_service import create_report
+from ..util.token_verify import token_required
 
 report_dto = ReportDto()
 _report = report_dto.report
@@ -9,6 +10,9 @@ _report = report_dto.report
 @ns.route("/report")
 class ReportList(Resource):
     @ns.expect(_report, validate=True)
-    def post(self):
+    @ns.doc(security="bearer")
+    @token_required
+    def post(self, decoded_token):
         """Creates a new Report"""
-        return create_report(ns.payload)
+        user_id = decoded_token["id"]
+        return create_report(ns.payload, user_id)
