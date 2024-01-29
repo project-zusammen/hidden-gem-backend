bc71ce39e1098b929dc2a77a3abdf1613607c595
Fatma Janna <fatma.janna@quinyx.com>
2024-01-29 10:00:47 +0700

    migrations

---
+++ migrations/versions/0d02a181f816_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0d02a181f816
+Revises: 885e3c0254fe, 04f99026bef6
+Create Date: 2024-01-29 09:06:47.794098
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0d02a181f816'
+down_revision = ('885e3c0254fe', '04f99026bef6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
