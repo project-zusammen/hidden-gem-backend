3b5df093e87da1e2a755cfb89c97faa017b58645
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-01-30 14:37:33 +0900

    add logged user check

--- app/main/controller/appeal_controller.py
+++ app/main/controller/appeal_controller.py
@@ -6,11 +6,19 @@ _appeal = appeal_dto.appeal
 from flask_restx import Resource
 from ..service.appeal_service import create_appeal
 from ...extensions import ns
+from ..util.token_verify import token_required
+from ..util.helper import error_handler
 
 
 @ns.route("/appeal")
 class AppealList(Resource):
+    @ns.doc(security="bearer")
+    @token_required
     @ns.expect(_appeal, validate=True)
-    def post(self):
+    def post(self, decoded_token):
+        role = decoded_token["role"]
+        if role != "user":
+            return error_handler("Access denied")
+        
         """Creates a new appeal"""
         return create_appeal(ns.payload)
