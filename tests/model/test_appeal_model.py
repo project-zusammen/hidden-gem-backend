362c6f5f098a9fd23c9801d0af2f0643918a8db3
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-02-05 05:36:37 +0900

    add test model

--- tests/model/test_appeal_model.py
+++ tests/model/test_appeal_model.py
@@ -3,12 +3,23 @@ import unittest
 from app.main import create_app
 from app.extensions import db
 from app.main.model.appeal import Appeal
+from app.main.model.user import User
 
 appeal_data = {
     "reason": "This is a test appeal.",
-    "report_id": "report_id",
+    "report_id": "report_id"
 }
 
+def register_user():
+    global user_id
+    user_data = {
+        "username": "test_user",
+        "email": "test_user@gmail.com",
+        "password": "test_password"
+    }
+    user_model = User()
+    user = user_model.register_user(user_data)
+    user_id = user_model.get_user_id(user["public_id"])
 
 class TestAppeal(unittest.TestCase):
     def setUp(self):
@@ -16,6 +27,7 @@ class TestAppeal(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
+        register_user()
 
     def tearDown(self):
         db.session.remove()
@@ -25,6 +37,7 @@ class TestAppeal(unittest.TestCase):
     def test_create_appeal(self):
         # ARRANGE
         appeal_model = Appeal()
+        appeal_data["user_id"] = user_id
 
         # ACT
         new_appeal = appeal_model.create_appeal(appeal_data)
@@ -32,3 +45,42 @@ class TestAppeal(unittest.TestCase):
         # ASSERT
         self.assertIsNotNone(new_appeal)
         self.assertEqual(new_appeal["reason"], appeal_data["reason"])
+
+    def test_get_all_appeals(self):
+        # ARRANGE
+        appeal_model = Appeal()
+        appeal_data["user_id"] = user_id
+        appeal_model.create_appeal(appeal_data)
+
+        # ACT
+        all_appeals = appeal_model.get_all_appeals()
+
+        # ASSERT
+        self.assertIsNotNone(all_appeals)
+        self.assertEqual(all_appeals[0]["reason"], appeal_data["reason"])
+
+    def test_get_appeal_by_id_role_admin(self):
+        # ARRANGE
+        appeal_model = Appeal()
+        appeal_data["user_id"] = user_id
+        appeal = appeal_model.create_appeal(appeal_data)
+
+        # ACT
+        retrieved_appeal = appeal_model.get_appeal_by_id(public_id=appeal["public_id"], user_id=user_id, role="admin")
+
+        # ASSERT
+        self.assertIsNotNone(retrieved_appeal)
+        self.assertEqual(retrieved_appeal["reason"], appeal_data["reason"])
+
+    def test_get_appeal_by_id_role_user(self):
+        # ARRANGE
+        appeal_model = Appeal()
+        appeal_data["user_id"] = user_id
+        appeal = appeal_model.create_appeal(appeal_data)
+
+        # ACT
+        retrieved_appeal = appeal_model.get_appeal_by_id(public_id=appeal["public_id"], user_id=user_id, role="user")
+
+        # ASSERT
+        self.assertIsNotNone(retrieved_appeal)
+        self.assertEqual(retrieved_appeal["reason"], appeal_data["reason"])
