c9407d4cbe1b578d9eb06d46eb43513915830827
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-02-08 05:56:36 +0900

    add get_all_reports and get_report_by_id

--- app/main/controller/report_controller.py
+++ app/main/controller/report_controller.py
@@ -1,7 +1,7 @@
 from ..util.dto import ReportDto
 from flask_restx import Resource
 from ...extensions import ns
-from ..service.report_service import create_report
+from ..service.report_service import create_report, get_all_reports, get_a_report
 from ..util.token_verify import token_required
 
 report_dto = ReportDto()
@@ -16,3 +16,24 @@ class ReportList(Resource):
         """Creates a new Report"""
         user_id = decoded_token["id"]
         return create_report(ns.payload, user_id)
+    
+    @ns.doc(security="bearer")
+    @token_required
+    def get(self, decoded_token):
+        # role = decoded_token["role"]
+        # if role != "admin":
+        #     return error_handler("Access denied")
+
+        """List all reports"""
+        return get_all_reports()
+    
+@ns.route("/report/<public_id>")
+@ns.param("public_id", "The report identifier")
+class Report(Resource):
+    @ns.doc(security="bearer")
+    @token_required
+    def get(self, decoded_token, public_id):
+        user_id = decoded_token["id"]
+        role = decoded_token["role"]
+        """Get a report by its identifier"""
+        return get_a_report(public_id, user_id, role)
