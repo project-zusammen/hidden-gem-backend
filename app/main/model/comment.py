bb43556498bce8a453c7841150970f1d3e79deb1
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-02-08 21:17:44 +0900

    remove duplicate

--- app/main/model/comment.py
+++ app/main/model/comment.py
@@ -10,7 +10,6 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     public_id = db.Column(db.String(100), unique=True, nullable=False)
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    # comment_id = db.Column(db.Integer, db.ForeignKey('comment.id')) -> yang bener
     review_id = db.Column(db.Integer, db.ForeignKey("review.id"), nullable=False)
     content = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
@@ -58,8 +57,6 @@ class Comment(db.Model):
 
             if not self.content:
                 raise Exception("Comment content is required")
-            if not self.content:
-                raise Exception("Comment content is required")
 
             self.created_at = datetime.datetime.utcnow()
             self.updated_at = datetime.datetime.utcnow()
@@ -77,12 +74,6 @@ class Comment(db.Model):
         except Exception as e:
             raise e
 
-    def get_all_comments(self):
-        try:
-            comments = self.query.filter_by(visible=True).all()
-            return [comment.serialize() for comment in comments]
-        except Exception as e:
-            raise e
 
     def get_all_comments(self):
         try:
