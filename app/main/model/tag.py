fa16ad0e846549aadc0a1b4dc20bdda5dc81dfb8
Achmadqizwini <akise.shun@gmail.com>
2024-02-10 14:38:18 +0700

    remove review_tag model and stuff

--- app/main/model/tag.py
+++ app/main/model/tag.py
@@ -30,13 +30,10 @@ class Tag(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    def create_tag(self, name):
+    def create_tag(self, data):
         try:
-            # tag = self.query.filter_by(name=name).first()
-            # if tag:
-            #     return {"id": tag.id}
             self.public_id = str(uuid.uuid4())
-            self.name = name
+            self.name = data.get('name')
             self.created_at = datetime.datetime.utcnow()
             self.updated_at = datetime.datetime.utcnow()
 
@@ -44,45 +41,13 @@ class Tag(db.Model):
             return self.serialize()
         except Exception as e:
             raise e
-
-
-class ReviewTag(db.Model):
-    __tablename__ = "review_tag"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    public_id = db.Column(db.String(100), unique=True)
-    review_id = db.Column(db.Integer, db.ForeignKey("review.id"), nullable=False)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f"<ReviewTag(tag_id={self.tag_id}, review_id={self.review_id})>"
-
-    def serialize(self):
-        created_at = convert_to_local_time(self.created_at)
-        updated_at = convert_to_local_time(self.updated_at)
-        return {
-            "public_id": self.public_id,
-            "tag_id": self.tag_id,
-            "review_id": self.review_id,
-            "created_at": created_at.isoformat() if self.created_at else None,
-            "updated_at": updated_at.isoformat() if self.updated_at else None,
-        }
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def create_review_tag(self, tag_id, review_id):
+    
+    def get_tag_db_id(self, public_id):
         try:
-            self.public_id = str(uuid.uuid4())
-            self.tag_id = tag_id
-            self.review_id = review_id
-            self.created_at = datetime.datetime.utcnow()
-            self.updated_at = datetime.datetime.utcnow()
-
-            self.save()
-            return self.serialize()
+            tag = self.query.filter_by(public_id=public_id).first()
+            if tag:
+                return tag.id
+            raise Exception("Tag not found. Invalid ID")
         except Exception as e:
             raise e
+
