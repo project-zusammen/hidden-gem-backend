02f400f45b9e45f4f42af9888720fe9b922e9a3d
Achmadqizwini <akise.shun@gmail.com>
2024-02-10 14:38:00 +0700

    add tag dto

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -89,3 +89,11 @@ class ReportDto:
             "reason": fields.String(required=True, description="report reason"),
         },
     )
+
+class TagDto:
+    tag = api.model(
+        "tag",
+        {
+            "name": fields.String(required=True, description="tag name"),
+        },
+    )
