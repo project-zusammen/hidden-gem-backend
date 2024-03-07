730852245e3a9829642870fec38360d90cd7e8ee
Achmadqizwini <akise.shun@gmail.com>
2024-03-08 01:49:32 +0700

    keep migratons file

---
+++ migrations/versions/b4b9b6ca76ce_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b4b9b6ca76ce
+Revises: d84fc4262100
+Create Date: 2024-01-29 08:54:28.017784
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4b9b6ca76ce'
+down_revision = 'd84fc4262100'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/bb7cb6913a68_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bb7cb6913a68
+Revises: 556f4fee7f4a
+Create Date: 2024-01-30 04:07:03.217163
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "bb7cb6913a68"
+down_revision = "556f4fee7f4a"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
